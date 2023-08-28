21802660abef26b712c9d37d4f854dd95ed5efde
Ghosted34 <greyman1934@outlook.com>
2023-08-29 00:40:49 +0100

    cleanup 1

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,7 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install 
 COPY . .
-
 ENV PORT=8080
-
+RUN npm run build
 EXPOSE 8080
-
-ENV NODE_ENV=$NODE_ENV
-
-CMD ["npm","start"]
+CMD ["npm", "start"]
--- src/app.ts
+++ src/app.ts
@@ -23,9 +23,9 @@ class App {
 
   // Configure Express middleware.
   private middleware(): void {
-    this.app.use(cookieParser());
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cookieParser());
     this.app.use(cors());
     this.app.use(helmet());
     this.app.use(morgan("dev"));
